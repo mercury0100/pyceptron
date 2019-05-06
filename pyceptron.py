7f67b036bbd54a737d326b3d822fcc7bfcf26047
Cooper <33894313+OmicronPi@users.noreply.github.com>
2019-05-06 12:02:55 +1000

    Update pyceptron.py

--- pyceptron.py
+++ pyceptron.py
@@ -1,10 +1,14 @@
 '''
 Pyceptron.py - a modular multi-layer feed-forward neural network library
+Arbitrary number of hidden layers with arbitrary number of neurons
+tanh, logistic and relu activation with an optional softmax output layer
+Weight decay, SGD momentum, mini-batch descent, dropout and cross-entropy loss are all available
 Author: Cooper Doyle
 '''
 import numpy as np
 
 class Activation(object):
+
     def __tanh(self, x):
         return np.tanh(x)
 
@@ -23,7 +27,7 @@ class Activation(object):
 
     def __relu_deriv(self,a,alpha=0.):
         # a = relu(x)
-        return np.where(a > 0, 1., 0.)
+        return np.where(a > 0, 1., 0.001)
 
     def __init__(self,activation='tanh'):
         if activation == 'logistic':
@@ -88,11 +92,14 @@ class HiddenLayer(object):
         self.grad_W = np.zeros(self.W.shape)
         self.grad_b = np.zeros(self.b.shape)
 
-    def forward(self, input):
+    def forward(self, input, dropout=None):
         '''
         :type input: numpy.array
         :param input: a symbolic tensor of shape (n_in,)
         '''
+        if dropout:
+            indices = np.random.choice(np.arange(input.size), replace=False, size=int(input.size * dropout))
+            input[indices] = 0
         lin_output = np.dot(input, self.W) + self.b
         self.output = (
             lin_output if self.activation is None
@@ -115,6 +122,24 @@ def get_mini_batches(X, y, batch_size):
     mini_batches = [(X_shuffled[i:i+batch_size,:], y_shuffled[i:i+batch_size]) for i in range(0, len(y), batch_size)]
     return mini_batches
 
+def soft_max(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+def cross_entropy(y, y_hat, epsilon=1e-12):
+    """
+    Computes cross entropy between targets (encoded as one-hot vectors)
+    and predictions.
+    Input: y_hat (N, k) ndarray
+           y (N, k) ndarray
+    Returns: scalar
+    """
+    y_hat = np.clip(y_hat, epsilon, 1. - epsilon)
+    N = y_hat.shape[0]
+    ce = -np.sum(y*np.log(y_hat+1e-9))/N
+    return ce
+
 class MLP:
     """
     """
@@ -134,17 +159,22 @@ class MLP:
         for i in range(len(layers)-1):
             self.layers.append(HiddenLayer(layers[i],layers[i+1],activation[i],activation[i+1]))
 
-    def forward(self,input):
+    def forward(self,input,dropout=None,softmax=False):
         for layer in self.layers:
-            output=layer.forward(input)
+            output=layer.forward(input, dropout=dropout)
             input=output
+        if softmax:
+            output = soft_max(output)
         return output
 
-    def criterion_MSE(self,y,y_hat):
+    def criterion_MSE(self,y,y_hat,xent=False):
         activation_deriv=Activation(self.activation[-1]).f_deriv
         # MSE
         error = y-y_hat
-        loss = 0.5*sum(error**2)
+        if not xent:
+            loss = 0.5*sum(error**2)
+        else:
+            loss = cross_entropy(y,y_hat)
         # calculate the delta of the output layer
         delta=-error*activation_deriv(y_hat)
         # return loss and delta
@@ -179,7 +209,7 @@ class MLP:
             self.layers[j].W += self.layers[j].V
             self.layers[j].b -= lr * db[j]
 
-    def fit(self,X,y,learning_rate=0.1, momentum=0., weight_decay=0., epochs=100, batch_size=None):
+    def fit(self,X,y,learning_rate=0.1, momentum=0., weight_decay=0., epochs=100, batch_size=8, dropout=None, softmax=False, xent=False):
         """
         Online learning.
         :param X: Input data or features
@@ -189,6 +219,10 @@ class MLP:
         :oaram weight_decay: sets the rate of weight decay between iterations, between 0 and 1
         :param epochs: number of times the dataset is presented to the network for learning
         :param batch_size: selects the batch size for mini-batch gradient descent. if 'None' then a more efficient loop is used for SGD.
+            NB: Stochastic gradient descent only works for output_activation = 'logistic'
+        :param dropout: sets the rate of dropout in the network during forward propagation.
+        :param softmax: Adds a softmax layer to the output
+        :param Xent: Employs cross-entropy loss function
         """
         X=np.array(X)
         y=np.array(y)
@@ -201,9 +235,9 @@ class MLP:
                 for it in range(X.shape[0]):
                     i=np.random.randint(X.shape[0])
                     # forward pass
-                    y_hat = self.forward(X[i])
+                    y_hat = self.forward(X[i], dropout=dropout, softmax=softmax)
                     # backward pass
-                    loss[it],delta=self.criterion_MSE(y[i],y_hat)
+                    loss[it],delta=self.criterion_MSE(y[i],y_hat,xent=xent)
                     self.backward(delta)
                     # update
                     self.update(learning_rate, momentum, weight_decay)
@@ -222,9 +256,9 @@ class MLP:
                     dW, db = [], []
                     for i in range(Xs.shape[0]):
                         # forward pass
-                        y_hat = self.forward(Xs[i])
+                        y_hat = self.forward(Xs[i], dropout=dropout, softmax=softmax)
                         # backward pass
-                        loss[i],delta=self.criterion_MSE(Ys[i],y_hat)
+                        loss[i],delta=self.criterion_MSE(Ys[i],y_hat,xent=xent)
                         self.backward(delta)
                         # fetch and store gradients
                         layer_grad_W, layer_grad_b = self.get_grads()
@@ -234,10 +268,15 @@ class MLP:
                     batch_loss[b] = np.mean(loss)
                     b += 1 # batch number increment
                     # calculate average batch gradients
-                    dW=np.array(dW).mean(axis=0)
-                    db=np.array(db).mean(axis=0)
+                    gradients_W = {}
+                    gradients_b = {}
+                    for i in range(len(dW[0])): #could replace with len(self.layers)
+                        gradients_W[i] = np.array([j[i] for j in dW]).mean(axis=0)
+                        gradients_b[i] = np.array([j[i] for j in db]).mean(axis=0)
+                    DW=[i for j,i in gradients_W.items()]
+                    Db=[i for j,i in gradients_b.items()]
                     # update weights with batch gradient
-                    self.batch_update(dW, db, learning_rate, momentum, weight_decay)
+                    self.batch_update(DW, Db, learning_rate, momentum, weight_decay)
                 to_return[k] = np.mean(batch_loss)
         return to_return
 
@@ -245,6 +284,11 @@ class MLP:
         x = np.array(x)
         output = []
         for i in np.arange(x.shape[0]):
-            output.append(self.forward(x[i,:]))
+            ls = self.forward(x[i,:]).tolist()
+            max_value=max(ls)
+            max_index = ls.index(max_value)
+            array = np.zeros(10)
+            array[max_index] = 1
+            output.append(array)
         np.array(output)
         return output
